3a734366c36d10c97e297b06c67168daf50ac443
Andreas Ekström <ekstrom.andreas@gmail.com>
2013-11-18 11:04:31 +0100

    Small improvements to show basic Meteor concepts

--- leaderboard.js
+++ leaderboard.js
@@ -3,6 +3,29 @@
 
 Players = new Meteor.Collection("players");
 
+Players.allow({
+  //insert: function() { return Meteor.user() },
+  update: function() { return true },
+});
+
+Meteor.methods({
+  add: function(player) {
+    var user = Meteor.user();
+
+    if (!user)
+      throw new Meteor.Error(401, "You need to login to add players");
+
+    if (!player.name)
+      throw new Meteor.Error(422, 'Please fill in a name');
+
+    player['timestamp'] = new Date().getTime()
+    player['author'] = Meteor.user().profile.name
+
+    var id = Players.insert(player);
+    return id;
+  }
+});
+
 if (Meteor.isClient) {
   Template.leaderboard.players = function () {
     return Players.find({}, {sort: {score: -1, name: 1}});
@@ -20,6 +43,20 @@ if (Meteor.isClient) {
   Template.leaderboard.events({
     'click input.inc': function () {
       Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+    },
+    'submit form': function(e) {
+      e.preventDefault();
+      console.log("hit");
+      var player = {
+        name: $(e.target).find('[name=name]').val(),
+        score: 5
+      }
+
+      Meteor.call('add', player, function (err, res) {
+        if (err)
+          return alert(err.reason);
+      });
+      $(e.target).find('[name=name]').val("");
     }
   });
 
@@ -28,6 +65,12 @@ if (Meteor.isClient) {
       Session.set("selected_player", this._id);
     }
   });
+
+  Meteor.subscribe('players');
+
+ Deps.autorun(function() {
+   console.log("There are " + Players.find().count() + ' players');
+ });
 }
 
 // On server startup, create some players if the database is empty.
@@ -43,5 +86,9 @@ if (Meteor.isServer) {
       for (var i = 0; i < names.length; i++)
         Players.insert({name: names[i], score: Math.floor(Random.fraction()*10)*5});
     }
+  });
+
+  Meteor.publish('players', function() {
+    return Players.find();
   });
 }
